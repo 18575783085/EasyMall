46f477946d28180b88aa085a187f86c3265dcc9a
oye <517553812@qq.com>
2017-10-08 17:13:26 +0800

    图片验证码---点击刷新

--- WebRoot/js/refreshCode.js
+++ WebRoot/js/refreshCode.js
@@ -1,4 +1,4 @@
 /* 点击图片刷新验证码 */
 function refreshcode(){
-	document.getElementById(verification).src="<%= request.getContextPath() %>/servlet/ValiImageServlet?flag="+Math.random();
+	document.getElementById("verification").src="<%= request.getContextPath() %>/servlet/ValiImageServlet?flag="+Math.random();
 }
